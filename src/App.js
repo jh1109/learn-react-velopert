bd187dbfa714700c64800148c83ba058e584f92e
Juhee Noh <nohjuhee227@gmail.com>
2023-02-16 17:46:40 +0900

    [learn] 3.4.2 함수 컴포넌트에서 useState 사용하기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import Counter from './Counter';
+import Say from './Say';
 
 const App = () => {
-  return <Counter />;
+  return <Say />;
 };
 
 export default App;
+
+const array = [
+  {id: 1, value: true},
+  {id: 2, value: true},
+  {id: 3, value: true},
+];
+
+let nextArray = array.concat({id: 4});
+console.log(nextArray);
+console.log(nextArray[0].id);
+const thirdArray = nextArray.filter(item=>item.id !== 2);
+console.log(thirdArray);
+const forthArray = thirdArray.map(item=>(item.id ===1 ? {...item, value: false} : item));
+console.log(forthArray);
