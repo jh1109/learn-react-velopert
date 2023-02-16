9d6fe4e8e3aaae2e4b547ebf3bca38fbc299c11f
Juhee Noh <nohjuhee227@gmail.com>
2023-02-17 00:56:33 +0900

    [learn] 4.2 이벤트 핸들링 익히기-input 여러개

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -2,23 +2,24 @@ import { Component } from 'react';
 
 class EventPractice extends Component {
   state = {
+    username: '',
     message: ''
   }
-  // constructor(props) {
-  //   super(props);
-  //   this.handleChange = this.handleChange.bind(this);
-  //   this.handleClick = this.handleClick.bind(this);
-  // }
 
-  // 임의 메서드를 화살표 함수로 작성하면 constructor에서 바인딩을 하지 않아도 됨.
   handleChange = (e) => {
-    this.setState({message: e.target.value});
+    this.setState({[e.target.name]: e.target.value});
   }
   handleClick = () => {
-    alert(this.state.message);
+    alert(this.state.username + ': ' + this.state.message);
     this.setState({
+      username: '',
       message: ''
     });
+  }
+  handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      this.handleClick();
+    }
   }
   render() {
     return (
@@ -26,10 +27,18 @@ class EventPractice extends Component {
         <h1>이벤트 연습</h1>
         <input  
           type="text"
+          name="username"
+          placeholder="사용자명"
+          value={this.state.username}
+          onChange={this.handleChange}
+          />
+        <input  
+          type="text"
           name="message"
           placeholder="아무거나 입력해 보세요."
           value={this.state.message}
           onChange={this.handleChange}
+          onKeyPress={this.handleKeyPress}
           />
           <button onClick={this.handleClick}>확인</button>
       </div>
