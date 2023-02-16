c14f69486577206ae9f6a5739d1b4e79df28a57c
Juhee Noh <nohjuhee227@gmail.com>
2023-02-17 01:57:08 +0900

    [learn] 함수 컴포넌트로 구현해 보기 - 객체 활용

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,15 +1,24 @@
 import { useState } from "react";
 
 const EventPractice = () => {
-  const [username, setUsername] = useState('');
-  const [message, setMessage] = useState('');
-
-  const onChangeUsername = e => setUsername(e.target.value);
-  const onChangeMessage = e => setMessage(e.target.value);
+  const [form, setForm] = useState({
+    username: '',
+    message: '',
+  });
+  const {username, message} = form;
+  const onChange = e => {
+    const nextForm = {
+      ...form,
+      [e.target.name]: e.target.value
+    };
+    setForm(nextForm);
+  }
   const onClick = () => {
     alert(username + ': ' + message);
-    setUsername('');
-    setMessage('');
+    setForm({
+      username: '',
+      message: ''
+    });
   };
   const onKeyPress = (e) =>{
     if (e.key === 'Enter') {
@@ -21,10 +30,10 @@ const EventPractice = () => {
     <div>
       <h1>이벤트 연습</h1>
       <input type="text" name="username" placeholder="사용자명" value={username}
-        onChange={onChangeUsername}
+        onChange={onChange}
       />
       <input type="text" name="message" placeholder="내용을 입력해주세요." value={message}
-        onChange={onChangeMessage} onKeyPress={onKeyPress}
+        onChange={onChange} onKeyPress={onKeyPress}
       />
       <button type="button" onClick={onClick}>확인</button>
     </div>
