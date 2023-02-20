5c452ab0856047fe573a895e0295ff749b17a6c3
Juhee Noh <nohjuhee227@gmail.com>
2023-02-20 11:48:13 +0900

    [learn] 5.3 컴포넌트에 ref달기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import { Component } from 'react';
-import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
 
 class App extends Component {
   render() {
-    return <ValidationSample />;
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToButtom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
   }
 }
 
